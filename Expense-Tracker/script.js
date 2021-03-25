51a9ea77abe0ce2009ed3c2fd659f9114b75ffc6
liuyihsuan <liuyihsuan1012@gmail.com>
2021-03-25 17:33:48 +0800

    finish JS Add & Delete Transactions part

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -15,6 +15,39 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+// 下方設定完事件監聽至此處撰寫function
+// 設定下方花費或淨賺提交處
+// Add transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (text.value.trim() === '' || amount.value.trim() === ''){
+        alert('Please add a text and amount');
+    } else {
+        const transaction ={
+            id: generateID(),
+            text: text.value,
+            // 影片提及此處須為數字
+            amount: +amount.value
+        };
+        
+        // console.log(transaction);確定輸入text及amount有數值
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValues();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+// 注意 生成ID 在後續刪除中見效
+// Generate random ID
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
     // Get sign
@@ -26,15 +59,16 @@ function addTransactionDOM(transaction) {
     // Add class based on value
    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
     // Math.abs 將負數轉為正數
+    // onclick="removeTransaction(${transaction.id})"注意
     item.innerHTML = `
         ${transaction.text} <span>${sign}${Math.abs(
             transaction.amount
-            )}</span> <button class="delete-btn">x</button>
+            )}</span> <button class="delete-btn"  onclick="removeTransaction(${transaction.id})">x</button>
     `;
 // 注意
     list.appendChild(item);
 }
-
+// 設定總數值，淨賺及花費數值
 // Upate the balance, income and expense
 function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
@@ -55,6 +89,14 @@ function updateValues() {
     money_plus.innerHTML = `$${income}`;
     money_minus.innerHTML = `$${expense}`; 
 }
+// 在設定完上方onclick後，接續此設定為讓history紀錄除去，以ID作為刪除的基礎
+// Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id
+        !== id);
+
+        init();
+}
 
 // 注意
 // Init app
@@ -66,3 +108,7 @@ function init() {
 }
 
 init ();
+
+// 設定事件監聽後 至上方撰寫function
+// not addTransactionDOM
+form.addEventListener('submit', addTransaction);
