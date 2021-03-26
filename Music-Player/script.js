69fb21a6ad16d5e12bdb2a26c3814a6bb1f4ab19
liuyihsuan <liuyihsuan1012@gmail.com>
2021-03-26 14:44:54 +0800

    done to JS play btn Event listeners part

--- Music-Player/script.js
+++ Music-Player/script.js
@@ -8,3 +8,27 @@ const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+// 呼應歌名，作為下方keep track of song轉換歌曲用
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+// ukulele=2，如轉換為1，歌曲會因此跳到summer
+// Keep track of song
+let songIndex = 2;
+// innertext再複習,const song->let songIndex->function loadSong
+// Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+  }
+
+  // Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+  
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  });
