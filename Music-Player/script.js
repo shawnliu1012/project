c7e1c500db9f3b436e2cc91cdc1679b392be2242
liuyihsuan <liuyihsuan1012@gmail.com>
2021-03-26 15:51:40 +0800

    done to JS event Click on progress bar part

--- Music-Player/script.js
+++ Music-Player/script.js
@@ -98,3 +98,6 @@ nextBtn.addEventListener('click', nextSong);
 
 // Time/song update
 audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
