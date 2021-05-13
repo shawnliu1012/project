6644cac6da84cca08024bff318e28ca3c63a6f67
liuyihsuan <liuyihsuan1012@gmail.com>
2021-05-13 23:40:48 +0800

    modified all

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -13,44 +13,40 @@ const amount = document.getElementById('amount')
 //     {id: 4, text: 'Camera', amount: 150 }
 // ];
 
-const localStorageTransactions = JSON.parse(localStorage.getItem
-    ('transactions'));
-// 從dummyTransactions改為: 目的為清零
-// let transactions = dummyTransactions;
-let transactions = 
-localStorage.getItem('transactions') !== null ?
-localStorageTransactions : [];
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 
-// 下方設定完事件監聽至此處撰寫function
-// 3.設定下方花費或淨賺提交處
+// let transactions = dummyTransactions;
+let transactions =
+    localStorage.getItem('transactions') !== null ?
+    localStorageTransactions : [];
+
+
 // Add transaction
 function addTransaction(e) {
     e.preventDefault();
 
-    if (text.value.trim() === '' || amount.value.trim() === ''){
+    if (text.value.trim() === '' || amount.value.trim() === '') {
         alert('Please add a text and amount');
     } else {
-        const transaction ={
+        const transaction = {
             id: generateID(),
             text: text.value,
-            // 影片提及此處須為數字
             amount: +amount.value
         };
-        
-        // console.log(transaction);確定輸入text及amount有數值
+
         transactions.push(transaction);
 
         addTransactionDOM(transaction);
 
         updateValues();
-// 在設定下方Update local storage transactions Function後設置
+
         updateLocalStorage();
 
         text.value = '';
         amount.value = '';
     }
 }
-// 注意 生成ID 在後續刪除中見效
+
 // Generate random ID
 function generateID() {
     return Math.floor(Math.random() * 100000000);
@@ -63,59 +59,55 @@ function addTransactionDOM(transaction) {
 
     const item = document.createElement('li');
 
-    // 注意
     // Add class based on value
-   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-    // Math.abs 將負數轉為正數
-    // onclick="removeTransaction(${transaction.id})"注意
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
     item.innerHTML = `
         ${transaction.text} <span>${sign}${Math.abs(
-            transaction.amount
-            )}</span> <button class="delete-btn"  onclick="removeTransaction(${transaction.id})">x</button>
+        transaction.amount
+    )}</span> <button class="delete-btn"  onclick="removeTransaction(${transaction.id})">x</button>
     `;
-// 注意
+
     list.appendChild(item);
 }
-// 2.設定總數值，淨賺及花費數值
+
 // Upate the balance, income and expense
 function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
 
     const total = amounts.reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-    // 注意
+        .toFixed(2);
+
     const income = amounts
         .filter(item => item > 0)
         .reduce((acc, item) => (acc += item), 0)
         .toFixed(2);
-    
+
     const expense = (
         amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
     ).toFixed(2);
 
     balance.innerHTML = `$${total}`;
     money_plus.innerHTML = `$${income}`;
-    money_minus.innerHTML = `$${expense}`; 
+    money_minus.innerHTML = `$${expense}`;
 }
-// 在設定完上方onclick後，接續此設定為讓history紀錄除去，以ID作為刪除的基礎
+
 // Remove transaction by ID
 function removeTransaction(id) {
-    transactions = transactions.filter(transaction => transaction.id
-        !== id);
-// 在設定Update local storage transactions 後設置
-        updateLocalStorage();
+    transactions = transactions.filter(transaction => transaction.id !==
+        id);
 
-        init();
+    updateLocalStorage();
+
+    init();
 }
-// 4.設定本地存儲
+
 // Update local storage transactions
 function updateLocalStorage() {
-    localStorage.setItem('transactions', JSON.stringify(transactions))
-    ;
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 
-// 注意
 // Init app
 function init() {
     list.innerHTML = '';
@@ -124,8 +116,7 @@ function init() {
     updateValues();
 }
 
-init ();
+init();
 
-// 設定事件監聽後 至上方撰寫function
 // not addTransactionDOM
 form.addEventListener('submit', addTransaction);
