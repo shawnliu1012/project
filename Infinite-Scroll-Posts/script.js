c01803bb2fe04c648810a42294bf66b450d594de
liuyihsuan <liuyihsuan1012@gmail.com>
2021-05-14 16:17:55 +0800

    modified JS

--- Infinite-Scroll-Posts/script.js
+++ Infinite-Scroll-Posts/script.js
@@ -8,19 +8,19 @@ let page = 1;
 // Fetch posts from API
 async function getPosts() {
     const res = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
     );
-  
+
     const data = await res.json();
-  
+
     return data;
-  }
+}
 
-  // Show posts in DOM
-  async  function showPosts() {
-      const posts = await getPosts();
+// Show posts in DOM
+async function showPosts() {
+    const posts = await getPosts();
 
-      posts.forEach(post => {
+    posts.forEach(post => {
         const postEl = document.createElement('div');
         postEl.classList.add('post');
         postEl.innerHTML = `
@@ -32,50 +32,50 @@ async function getPosts() {
         `;
 
         postsContainer.appendChild(postEl);
-  });
+    });
 }
 
 // Show loader & fetch more posts
 function showLoading() {
     loading.classList.add('show');
-  
+
     setTimeout(() => {
         // 讓下方loading點點消失
-      loading.classList.remove('show');
-  
-      setTimeout(() => {
-        page++;
-        showPosts();
-      }, 300);
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
     }, 1000);
-  }
+}
 
 // Filter posts by input
 function filterPosts(e) {
     const term = e.target.value.toUpperCase();
     const posts = document.querySelectorAll('.post');
-  
+
     posts.forEach(post => {
-      const title = post.querySelector('.post-title').innerText.toUpperCase();
-      const body = post.querySelector('.post-body').innerText.toUpperCase();
-//   如果符合就顯示 沒有就無
-      if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
-        post.style.display = 'flex';
-      } else {
-        post.style.display = 'none';
-      }
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+        //   如果符合就顯示 沒有就無
+        if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+            post.style.display = 'flex';
+        } else {
+            post.style.display = 'none';
+        }
     });
-  }
+}
 
 //Show initial posts
 showPosts();
 
 window.addEventListener('scroll', () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
-      showLoading();
-    }
-  });
 
-  filter.addEventListener('input', filterPosts);
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        showLoading();
+    }
+});
+
+filter.addEventListener('input', filterPosts);
